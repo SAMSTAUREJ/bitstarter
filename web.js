2a43f9990bb2e7e2508db01cada4d86a7b8f5e89
SAMSTAUREJ <samstaurejsiddiquie@gmail.com>
2013-07-20 04:28:41 -0700

    first modification

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buff = new Buffer(30);
+
+var infile = "index.html"
+
+buff.readFileSync(infile);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
